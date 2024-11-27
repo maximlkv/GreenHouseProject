777b1cc684dd669bd90f147da6467c5032ed2732
Sol <sol.orive@gmail.com>
2024-11-27 13:07:24 +0100

    Adding actuators to GUI

--- template code/project-2024/src/main/java/no/ntnu/controlpanel/ControlPanelCommunicationChannel.java
+++ template code/project-2024/src/main/java/no/ntnu/controlpanel/ControlPanelCommunicationChannel.java
@@ -7,6 +7,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.*;
 
+import no.ntnu.greenhouse.Actuator;
+import no.ntnu.greenhouse.SensorActuatorNode;
 import no.ntnu.greenhouse.SensorReading;
 import no.ntnu.run.ControlPanelStarter;
 import no.ntnu.tools.Logger;
@@ -20,6 +22,7 @@ public class ControlPanelCommunicationChannel implements CommunicationChannel {
     private final String serverAddress;
     private final int serverPort;
     private boolean isOpen;
+
 
     public ControlPanelCommunicationChannel(String serverAddress, int serverPort) {
         this.serverAddress = serverAddress;
@@ -69,14 +72,18 @@ public class ControlPanelCommunicationChannel implements CommunicationChannel {
                 while ((message = socketReader.readLine()) != null) {
                     Logger.info("Received sensor data: " + message);
                     JSONObject jsonObject = new JSONObject(message);
-                    int nodeId = jsonObject.getInt("source");
+                    int nodeId = jsonObject.getInt("id");
                     Logger.info("Adding node info to GUI:"+ nodeId);
-                    logic.onNodeAdded(new SensorActuatorNodeInfo(nodeId));
+                    SensorActuatorNodeInfo sensorActuatorNodeInfo = new SensorActuatorNodeInfo(nodeId);
+                    logic.onNodeAdded(sensorActuatorNodeInfo);
 
                     // Sensor Data updates.
-                    // TODO we need to update this message before
-                   // List<SensorReading> sensors = parseSensorReadings(message);
-                   // logic.onSensorData(nodeId, sensors);
+                    List<SensorReading> sensors = parseSensorReadings(message);
+                    List<Actuator> actuators = parseActuators(message,sensorActuatorNodeInfo);
+                    logic.onSensorData(nodeId, sensors);
+                    for (Actuator actuator : actuators) {
+                        logic.onActuatorStateChanged(nodeId, actuator.getId(), actuator.isOn());
+                    }
                 }
             } catch (IOException e) {
                 Logger.error("Error reading from server: " + e.getMessage());
@@ -98,19 +105,48 @@ public class ControlPanelCommunicationChannel implements CommunicationChannel {
     private List<SensorReading> parseSensorReadings(String sensorInfo) {
         List<SensorReading> sensorReadings = new ArrayList<>();
 
-        JSONArray jsonArray = new JSONArray(sensorInfo);
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
+        // Parsear el mensaje JSON
+        JSONObject jsonObject = new JSONObject(sensorInfo);
 
-            String type = jsonObject.getString("type");
-            double value = jsonObject.getDouble("value");
-            String unit = jsonObject.getString("unit");
+        // Extraer el array "sensors"
+        JSONArray sensorsArray = jsonObject.getJSONArray("sensors");
 
-            SensorReading reading = new SensorReading(type, value, unit);
-            sensorReadings.add(reading);
+        // Iterar sobre los sensores
+        for (int i = 0; i < sensorsArray.length(); i++) {
+            JSONObject sensorObject = sensorsArray.getJSONObject(i);
+
+            String type = sensorObject.getString("type");
+            double value = sensorObject.getDouble("value");
+            String unit = sensorObject.getString("unit");
+
+            sensorReadings.add(new SensorReading(type, value, unit));
         }
 
         return sensorReadings;
+    }
+    public static List<Actuator> parseActuators(String jsonMessage, SensorActuatorNodeInfo info) {
+        List<Actuator> actuators = new ArrayList<>();
+
+        JSONObject jsonObject = new JSONObject(jsonMessage);
+        JSONArray actuatorsArray = jsonObject.getJSONArray("actuators");
+
+        for (int i = 0; i < actuatorsArray.length(); i++) {
+            JSONObject actuatorObject = actuatorsArray.getJSONObject(i);
+
+            int id = actuatorObject.getInt("id");
+            String type = actuatorObject.getString("type");
+            String status = actuatorObject.getString("status");
+
+          //  actuators.add(new Actuator(type,id));
+            Actuator actuator = new Actuator(type, info.getId());
+          //  actuator.setListener(logic);
+            info.addActuator(actuator);
+
+        }
+
+        return actuators;
+    }
+
+
 
     }
-}
